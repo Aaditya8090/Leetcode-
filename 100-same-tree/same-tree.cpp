9cf4869f7e95de12287fc315cf66005412788e88
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-02-13 11:14:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (95.97%) - LeetSync

---
+++ 100-same-tree/same-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool good(TreeNode*p, TreeNode *q){
+        if(p == nullptr && q == nullptr)
+            return true;
+        if(p == nullptr || q == nullptr)
+            return false;
+        return ((p->val == q->val) && good(p->left, q->left) && good(p->right, q->right));
+        
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return good(p, q);
+    }
+};
