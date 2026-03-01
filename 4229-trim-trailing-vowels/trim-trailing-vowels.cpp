ceb1b026f8af8b761260d50b63373a5c2ecc40ca
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-03-02 01:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (100.00%) - LeetSync

--- 4229-trim-trailing-vowels/trim-trailing-vowels.cpp
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string trimTrailingVowels(string s) {
-        unordered_set<char>st = {'a', 'e', 'i', 'o', 'u'};
+        // unordered_set<char>st = {'a', 'e', 'i', 'o', 'u'};
         int n = s.size();
         for(int i=n-1; i>=0; i--){
-            if(st.count(s[i])){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
                 s.pop_back();
-            }else
+            else
                 break;
         }
         return s;
