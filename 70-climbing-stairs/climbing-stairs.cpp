7cc0241f92e53f7e438160cfce52d52fb86a3d43
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-03-06 01:37:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,11 +3,21 @@ public:
     int climbStairs(int n) {
         // 1-D dp
         
-        // first approach  -> O(n) time  and O(1) space
-        vector<int>dp(n+1,0);
-        dp[0] = dp[1] = 1; 
-        for(int i=2; i<=n; i++)
-            dp[i] = dp[i-1]+dp[i-2];
-        return dp[n];
+        // first approach  -> O(n) time  and O(n) space
+        // vector<int>dp(n+1,0);
+        // dp[0] = dp[1] = 1; 
+        // for(int i=2; i<=n; i++)
+        //     dp[i] = dp[i-1]+dp[i-2];
+        // return dp[n];
+
+
+        // second approach  -> O(n) time and O(1) space
+        int prev1=1, prev2=1;
+        for(int i=2; i<=n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
