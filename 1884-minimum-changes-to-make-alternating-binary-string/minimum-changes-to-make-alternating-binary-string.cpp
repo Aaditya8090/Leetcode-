3cfa7eaa29841d60fddba86121513130a04de1a9
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-03-05 19:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (76.20%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.size();
+        int cnt1=0, cnt2=0;
+        for(int i=0; i<n; i++)
+            if((i%2==0 && s[i] == '1') || (i%2==1 && s[i]=='0'))
+                cnt1++;
+        for(int i=0; i<n; i++)
+            if((i%2==0 && s[i] == '0') || (i%2==1 && s[i]=='1'))
+                cnt2++;
+        
+        return min(cnt1, cnt2);
+    }
+};
