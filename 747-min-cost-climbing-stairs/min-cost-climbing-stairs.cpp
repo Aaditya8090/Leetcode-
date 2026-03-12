e3c80f07d4ac11472604490bc3b31f794db71ba0
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-03-12 14:27:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (72.13%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        if(n<=1)
-            return min(cost[0], cost[1]);
+
         int prev1=0, prev2 = 0;
         for(int i=2; i<=n; i++){
             int curr = min(prev1+cost[i-1], prev2+cost[i-2]);
