5a0e3355424fdad1a42bb7df9d449ec58b5b582a
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-03-28 03:27:50 +0530

    Time: 3 ms (82.32%) | Memory: 17.4 MB (5.23%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -8,26 +8,25 @@
  * };
  */
 class Solution {
-    vector<int>ans;
-    unordered_map<TreeNode* ,TreeNode*>par;
+    unordered_map<TreeNode*, vector<TreeNode*>>g;
 
-    void pre(TreeNode *root){
-        if(!root)
+    void build(TreeNode *node, TreeNode *parent){
+        if(!node)
             return;
         
-        if(root->left){
-            par[root->left] = root;
-            pre(root->left);
+        if(parent){
+            g[node].push_back(parent);
+            g[parent].push_back(node);
         }
-        if(root->right){
-            par[root->right] = root;
-            pre(root->right);
-        }
+        
+        build(node->left, node);
+        build(node->right, node);
     }
 
-    void bfs(TreeNode *root, int &k){
-        if(!root) return;
+    vector<int> bfs(TreeNode *root, int &k){
+        if(!root) return {};
 
+        vector<int>ans;
         unordered_set<TreeNode*>visited;
         queue<TreeNode*>q;
         
@@ -36,39 +35,98 @@ class Solution {
 
         int level=0;
         while(!q.empty()){
-            int size =  q.size();
+            int size = q.size();
             for(int i=0; i<size; i++){
                 auto curr = q.front();
                 q.pop();
 
-                if(level == k)
-                    ans.push_back(curr->val);
+                ans.push_back(curr->val);
 
-                if(curr->left && visited.count(curr->left)==0){
-                    visited.insert(curr->left);
-                    q.push(curr->left);
-                }
-
-                if(curr->right && visited.count(curr->right)==0){
-                    visited.insert(curr->right);
-                    q.push(curr->right);
-                }
-
-                if(par.count(curr) && visited.count(par[curr])==0){
-                    visited.insert(par[curr]);
-                    q.push(par[curr]);
+                for(auto fr: g[curr] ){
+                    if(!visited.count(fr)){
+                        visited.insert(fr);
+                        q.push(fr);
+                    }
                 }
             }
-
-            if(level == k) break;
+            
+            if(level == k) return ans;
+            ans.clear();
             level++;
         }
+        return ans;
     }
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         // map child to it parent to convert it into graph
-        pre(root);
-        bfs(target, k);
-        return ans;
+        build(root, nullptr);
+        return bfs(target, k);
     }
 };
+
+// little diff way
+// class Solution {
+//     vector<int>ans;
+//     unordered_map<TreeNode* ,TreeNode*>par;
+
+//     void pre(TreeNode *root){
+//         if(!root)
+//             return;
+        
+//         if(root->left){
+//             par[root->left] = root;
+//             pre(root->left);
+//         }
+//         if(root->right){
+//             par[root->right] = root;
+//             pre(root->right);
+//         }
+//     }
+
+//     void bfs(TreeNode *root, int &k){
+//         if(!root) return;
+
+//         unordered_set<TreeNode*>visited;
+//         queue<TreeNode*>q;
+
+//         q.push(root);
+//         visited.insert(root);
+
+//         int level=0;
+//         while(!q.empty()){
+//             int size =  q.size();
+//             for(int i=0; i<size; i++){
+//                 auto curr = q.front();
+//                 q.pop();
+
+//                 if(level == k)
+//                     ans.push_back(curr->val);
+
+//                 if(curr->left && visited.count(curr->left)==0){
+//                     visited.insert(curr->left);
+//                     q.push(curr->left);
+//                 }
+
+//                 if(curr->right && visited.count(curr->right)==0){
+//                     visited.insert(curr->right);
+//                     q.push(curr->right);
+//                 }
+
+//                 if(par.count(curr) && visited.count(par[curr])==0){
+//                     visited.insert(par[curr]);
+//                     q.push(par[curr]);
+//                 }
+//             }
+
+//             if(level == k) break;
+//             level++;
+//         }
+//     }
+// public:
+//     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+//         // map child to it parent to convert it into graph
+//         pre(root);
+//         bfs(target, k);
+//         return ans;
+//     }
+// };
