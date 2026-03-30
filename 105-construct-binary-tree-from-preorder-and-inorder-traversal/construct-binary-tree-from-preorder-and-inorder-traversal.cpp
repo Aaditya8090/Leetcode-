8390e36634a594db838974a36e7ff1add6e0319d
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-03-31 01:38:48 +0530

    Time: 2 ms (79.56%) | Memory: 27.4 MB (53.10%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,31 +9,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Best Sol
 class Solution {
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int &idx){
-        if(start>end)
-            return nullptr;
-        
-        int rootVal = preorder[idx];
+    unordered_map<int,int> mp;
+public:
+    int preIndex = 0;
 
-        int i=start;
-        for(; i<end; i++){
-            if(inorder[i] == rootVal)
-                break;
-        }
-        ++idx;
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int inStart, int inEnd) {
+        if(inStart > inEnd) return NULL;
 
-        TreeNode *root = new TreeNode(rootVal);
-        root->left = solve(preorder, inorder, start, i-1, idx);
-        root->right = solve(preorder, inorder, i+1, end, idx);
+        int rootVal = preorder[preIndex++];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int inIndex = mp[rootVal];
+
+        root->left = build(preorder, inorder, inStart, inIndex - 1);
+        root->right = build(preorder, inorder, inIndex + 1, inEnd);
 
         return root;
     }
-public:
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = inorder.size();
+        for(int i = 0; i < inorder.size(); i++)
+            mp[inorder[i]] = i;
 
-        int idx=0;
-        return solve(preorder, inorder, 0, n-1, idx);
+        return build(preorder, inorder, 0, n-1);
     }
 };
+
+
+// similar approach but little diff
+
+// class Solution {
+//     TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int &idx){
+//         if(start>end)
+//             return nullptr;
+        
+//         int rootVal = preorder[idx];
+
+//         int i=start;
+//         for(; i<end; i++){
+//             if(inorder[i] == rootVal)
+//                 break;
+//         }
+//         ++idx;
+
+//         TreeNode *root = new TreeNode(rootVal);
+//         root->left = solve(preorder, inorder, start, i-1, idx);
+//         root->right = solve(preorder, inorder, i+1, end, idx);
+
+//         return root;
+//     }
+// public:
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         int n = inorder.size();
+
+//         int idx=0;
+//         return solve(preorder, inorder, 0, n-1, idx);
+//     }
+// };
