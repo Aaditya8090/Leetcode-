a3932cc79d446d6c5f5543a6108b29c2a99e34ff
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-07-26 15:03:29 +0530

    Time: 16 ms (18.84%) | Memory: 31.6 MB (46.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -4,8 +4,7 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         int ans1 = nums[n-1]*nums[n-2]*nums[n-3];
-        int ans2 = nums[0]*nums[1]*nums[2];
-        int ans3 = nums[n-1]*nums[0]*nums[1];   // only one pos
-        return max(ans1, max(ans2, ans3));
+        int ans2 = nums[0]*nums[1]*nums[n-1];
+        return max(ans1, ans2);
     }
 };
