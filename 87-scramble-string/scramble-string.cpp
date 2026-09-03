c084e8101275ea389fdcc2b2dc4d721f7e2c43f2
Aaditya8090 <145319049+Aaditya8090@users.noreply.github.com>
2026-09-03 21:59:40 +0530

    Time: 15 ms (58.92%) | Memory: 16.4 MB (67.72%) - LeetSync

--- 87-scramble-string/scramble-string.cpp
+++ 87-scramble-string/scramble-string.cpp
@@ -13,8 +13,19 @@ public:
 
         if(mp.find(key) != mp.end())
             return mp[key];
-        
+
         int n = s1.size();
+        
+        // 1. Pruning optimization to avoid TLE
+        int count[26] = {0};
+        for (int i = 0; i < n; i++) {
+            count[s1[i] - 'a']++;
+            count[s2[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return mp[key] = false;
+        }
+        
         bool flag = false;
         for(int i=1; i<n; i++){
             // swap
